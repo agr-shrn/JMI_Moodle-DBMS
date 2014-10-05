dbc136d3b58a57a08b4559610baac93e47cd325e
Sharan Agrawal <sharan.jmi@gmail.com>
2014-10-05 19:13:42 +0530

    fixed database issues

--- public/admin_login.php
+++ public/admin_login.php
@@ -1,29 +1,24 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
 <?php
 
-    include_once("../includes/functions.php");
-    include_once("../includes/session.php");
-     $host = "localhost";
-      $user = "root";
-      $password = "12345";
-      $db = "jmi_moodle";
-      $r = mysql_connect($host, $user, $password);
-  
-    mysql_select_db($db);
- 
     $user = $_POST['username'];
     $password = $_POST['password'];
 
+    
     if(isset($_POST['submit']) && isset($user) && isset($password)){
         $query = "SELECT * from admin";
-        $rs = mysql_query($query);
+        $rs = mysqli_query($connection, $query);
         confirm_query($rs);
-        $row = mysql_fetch_row($rs);
+        $row = mysqli_fetch_row($rs);
         if($user === $row[0] && $password === $row[1]){
             $_SESSION['loggedin'] = 1;
             $_SESSION['user_id'] = $user;
             redirect_to("../template/admin_home.php");
         }
     } 
-        redirect_to("../template/homepage.php");
+
+    redirect_to("../template/homepage.php");
     
 ?>
